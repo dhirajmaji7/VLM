494c0272b6ff877ef6e963a2f4f190cf995adfbe
Dhiraj Maji <dhirajmaji7@gmail.com>
2025-07-18 16:45:02 -0500

    Merge branch 'dmaji/dev'

---
+++ clip/main.py
@@ -0,0 +1,21 @@
+import torch
+
+from model import CLIPModel
+from tokenizer import CLIPTextTokenizer
+from loss import InfoNCECriterion
+from train import Trainer
+from config import CLIPConfig
+
+
+if __name__ ==  "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize components
+    config = CLIPConfig()
+    tokenizer = CLIPTextTokenizer(config)
+    model = CLIPModel(config)
+    criterion = InfoNCECriterion()
+
+    trainer = Trainer(config, model, criterion, device, tokenizer)
+    trainer.run()
+    
--- clip/tokenizer.py
+++ clip/tokenizer.py
@@ -1,17 +1,18 @@
 import torch
 import tiktoken
 
+from config import CLIPConfig
 class CLIPTextTokenizer:
-    def __init__(self, context_length=77):
-        self.tokenizer = tiktoken.get_encoding("gpt2")
-        self.bos_token_id = 49406  # <|startoftext|> used in CLIP
-        self.eos_token_id = 49407  # <|endoftext|> used in CLIP
+    def __init__(self, config: CLIPConfig):
+        self.config = config
+        self.tokenizer = tiktoken.get_encoding(config.tokenizer_name)
+        self.bos_token_id = config.bos_token_id 
+        self.eos_token_id = config.eos_token_id
+        self.context_length = config.context_length
         self.n_vocab = self.tokenizer.n_vocab
-        self.context_length = context_length
-        print(f"Using tokenizer: {self.tokenizer.name} with vocab size: {self.n_vocab}")
 
     def tokenize_text(self, text):
         token_ids = self.tokenizer.encode(text)
-        token_ids = [self.bos_token_id] + token_ids[:self.context_length-2] + [self.eos_token_id]
+        token_ids = [self.bos_token_id] + token_ids[:self.context_length - 2] + [self.eos_token_id]
         padded = token_ids + [0] * (self.context_length - len(token_ids))
         return torch.tensor(padded, dtype=torch.long)
